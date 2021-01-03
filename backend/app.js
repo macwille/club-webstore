0377ab7cc701ff4ff324776da90f9d1d19dea354
macwille <vw.manninen@gmail.com>
2021-01-03 09:11:24 +0200

    Set up middleware and mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const config = require('./util/config')
+const middleware = require('./util/middleware')
+const logger = require('./util/logger')
+const mongoose = require('mongoose')
 
-app.use(express.json())
+logger.info('connecting to Mongoose')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+if (process.env.NODE_ENV === 'test') {
+  logger.info('Testing mode detected')
+}
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const app = require('./app')
 const http = require('http')
 const config = require('./util/config')
+const logger = require('./util/logger')
 
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
